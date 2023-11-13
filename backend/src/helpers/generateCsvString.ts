cd313052794c76ada12a82b8777785dc2742deb7
Marin Teskera <marin.teskera@infinum.com>
2023-11-13 20:35:58 +0100

    csv download works

--- backend/src/helpers/generateCsvString.ts
+++ backend/src/helpers/generateCsvString.ts
@@ -1,32 +1,25 @@
+import { Parser } from "@json2csv/plainjs";
 import { ICsvData } from "../interfaces/csv.interface";
 
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+export const generateCsvString = (data: Array<ICsvData>) => {
+  const header = [
+    { id: "name", title: "Name" },
+    { id: "color", title: "Color" },
+    { id: "type", title: "Type" },
+    { id: "description", title: "Description" },
+    { id: "countryname", title: "CountryName" },
+    { id: "countrycode", title: "CountryCode" },
+    { id: "nutritionalvaluename", title: "NutritionalValueName" },
+    {
+      id: "nutritionalValuePercentage",
+      title: "NutritionalValuePercentage",
+    },
+    { id: "price", title: "Price" },
+    { id: "currency", title: "Currency" },
+  ];
 
-export const generateCsvString = async (data: Array<ICsvData>) => {
-  const csvWriter = createCsvWriter({
-    path: "temp.csv",
-    header: [
-      { id: "name", title: "Name" },
-      { id: "color", title: "Color" },
-      { id: "type", title: "Type" },
-      { id: "description", title: "Description" },
-      { id: "countryname", title: "CountryName" },
-      { id: "countrycode", title: "CountryCode" },
-      { id: "nutritionalvaluename", title: "NutritionalValueName" },
-      {
-        id: "nutritionalValuePercentage",
-        title: "NutritionalValuePercentage",
-      },
-      { id: "price", title: "Price" },
-      { id: "currency", title: "Currency" },
-    ],
-  });
+  const parser = new Parser();
+  const csv = parser.parse(data);
 
-  console.log(1);
-
-  await csvWriter.writeRecords(data);
-
-  console.log(csvWriter);
-
-  return csvWriter.stringify();
+  return csv;
 };
