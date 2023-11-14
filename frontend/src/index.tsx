196f4877099b15a4fa1c4f6e403932313fb3fff3
Marin Teskera <marin.teskera@infinum.com>
2023-11-14 02:16:43 +0100

    change router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,24 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createHashRouter as createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Database } from "./Database";
 import { Home } from "./Home";
-import ErrorPage from "./error-page";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  { path: "/database", element: <Database /> },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//     errorElement: <ErrorPage />,
+//   },
+//   { path: "/database", element: <Database /> },
+// ]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -26,7 +22,16 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <RouterProvider router={router} />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/database">
+            <Database />
+          </Route>
+        </Switch>
+      </Router>
     </ChakraProvider>
   </React.StrictMode>
 );
