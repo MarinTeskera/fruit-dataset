aac8ec0981e91b397c1550c4d3ad85a50ecd0465
Marin Teskera <marin.teskera@infinum.com>
2023-12-18 18:17:32 +0100

    change response codes

--- backend/src/api.route.ts
+++ backend/src/api.route.ts
@@ -5,87 +5,91 @@ import { ICreateData } from "./interfaces/create.interface";
 import { IUpdateData } from "./interfaces/update.interface";
 import { updateFruit } from "./helpers/update";
 import { deleteFruit } from "./helpers/delete";
-const db = require("../db");
 
+const db = require("../db");
 const router = Router();
 
 router.get("/fruits", async (req, res) => {
   try {
     const response = await getJSON("", "");
-    res.send(response);
+    res.status(200).json({ data: response });
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// GET - Dohvaćanje pojedinačnog resursa
 router.get("/fruits/:id", async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   try {
     const response = await getJSON(`${id}`, "id");
-    res.send(response);
+    if (!response.length) {
+      res.status(404).json({ error: "Fruit not found" });
+    } else {
+      res.status(200).json({ data: response[0] });
+    }
   } catch (err) {
     console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// GET - Dodatne krajnje točke
 router.get("/fruits/color/:color", async (req, res) => {
   const { color } = req.params;
   try {
     const response = await getJSON(color, "color");
-    res.send(response);
+    res.status(200).json({ data: response });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// GET - Dodatne krajnje točke
 router.get("/fruits/countryCode/:countryCode", async (req, res) => {
   const { countryCode } = req.params;
   try {
     const response = await getJSON(countryCode, "countryCode");
-    res.send(response);
+    res.status(200).json({ data: response });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// POST - Ubacivanje pojedinačnog resursa
 router.post("/fruits", async (req, res) => {
   const data: ICreateData = req.body;
   try {
     await createFruit(data);
-    res.status(201).json({ data: "Fruit created" });
+    res.status(201).json({ data: "Fruit created successfully" });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// PUT - Osvježavanje elemenata resursa
 router.put("/fruits/:id", async (req, res) => {
   const data: IUpdateData = req.body;
   const { id } = req.params;
   try {
     const fruitId = parseInt(id);
     await updateFruit(fruitId, data);
-    res.json({ data: "fruit updated" });
+    res.status(200).json({ data: "Fruit updated successfully" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-// DELETE - Brisanje pojedinog resursa
 router.delete("/fruits/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const fruitId = parseInt(id);
     await deleteFruit(fruitId);
-    res.status(200).json({ data: "fruit deleted" });
+    res.status(200).json({ data: "Fruit deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
+});
+
+// Handle non-existent API endpoints
+router.use((req, res) => {
+  res.status(404).json({ error: "Endpoint not found" });
 });
 
 export default router;
