c6fe9ba78afd103d53f3e40cf61e57a60797d2d3
Marin Teskera <marin.teskera@infinum.com>
2023-12-18 17:38:10 +0100

    delete works

--- backend/src/api.route.ts
+++ backend/src/api.route.ts
@@ -4,6 +4,7 @@ import { createFruit } from "./helpers/create";
 import { ICreateData } from "./interfaces/create.interface";
 import { IUpdateData } from "./interfaces/update.interface";
 import { updateFruit } from "./helpers/update";
+import { deleteFruit } from "./helpers/delete";
 const db = require("../db");
 
 const router = Router();
@@ -79,18 +80,9 @@ router.put("/fruits/:id", async (req, res) => {
 router.delete("/fruits/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await db.query(
-      "DELETE FROM fruit WHERE id = $1 RETURNING *",
-      [id]
-    );
-    if (result.rows.length === 0) {
-      res.status(404).json({ error: "Resource not found" });
-    } else {
-      res.json({
-        data: result.rows[0],
-        message: "Resource deleted successfully",
-      });
-    }
+    const fruitId = parseInt(id);
+    await deleteFruit(fruitId);
+    res.status(200).json({ data: "fruit deleted" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
